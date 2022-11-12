720c8e3da2305519c74b24dd72dbc348e1d3ce9e
JoshHerty <joshuaherty7@gmail.com>
2022-11-12 15:56:46 -0500

    initial commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+import logo from "../assets/Tesla-logo.png";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="Header">
+      <button className="menu-selector">Menu</button>
+    </div>
+  );
+};
+
+export default Header;
